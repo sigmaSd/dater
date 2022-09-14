c28f81f750890c23d4d6003a82af751e3a403ade
Nbiba Bedis <bedisnbiba@gmail.com>
2022-09-14 08:01:15 +0100

    handle fixed holidays

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,60 @@
 const week = 7;
 const month = week * 4;
 
+// year is not relevant
+const holidays = [
+  {
+    name: "Jour de l'an",
+    date: new Date("2022-01-01"),
+  },
+  {
+    name: "Fête de l’Indépendance",
+    date: new Date("2022-03-20"),
+  },
+  {
+    name: "Journée des Martyrs",
+    date: new Date("2022-04-09"),
+  },
+  {
+    name: "Fête du Travail",
+    date: new Date("2022-05-01"),
+  },
+  {
+    name: "Fête du la Femme",
+    date: new Date("2022-08-13"),
+  },
+  {
+    name: "Fête de l’Evacuation",
+    date: new Date("2022-10-15"),
+  },
+  {
+    name: "Fête de la révolution",
+    date: new Date("2022-12-17"),
+  },
+];
+const isHoliday = (date: Date) => {
+  const day = date.getDate();
+  const month = date.getMonth();
+
+  return holidays.find((holiday) => {
+    return holiday.date.getDate() == day && holiday.date.getMonth() == month;
+  });
+};
+
 const DateDisplay = ({ name, date }: { name: string; date: Date }) => {
+  const maybeHoliday = isHoliday(date);
+
   return (
-    <div>
-      <h1 class="font-bold">{name}</h1>
-      <p class="text-blue-600 font-bold text-xl">{date.toDateString()}</p>
+    <div class="flex flex-col">
+      <h1 class="font-bold place-self-center">{name}</h1>
+      {maybeHoliday
+        ? (
+          <div class="font-bold text-xl flex gap-2">
+            <p class="text-blue-600">{date.toDateString()}</p>
+            <p class="text-red-500">{maybeHoliday.name}</p>
+          </div>
+        )
+        : <p class="text-blue-600 font-bold text-xl">{date.toDateString()}</p>}
     </div>
   );
 };
